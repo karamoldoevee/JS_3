7a368715103d1103d45d54e62302d4b1a997dc98
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-11-28 19:04:35 +0600

    Добавлен JS для вывода стран

---
+++ second/main.js
@@ -0,0 +1,47 @@
+const indexLink = "https://restcountries.eu/rest/v2/all?fields=name;alpha3Code"
+
+
+function renderData(data){
+	const container = $(".container");
+	for (let i = 0; i < data.length; i++){
+		let countryDiv = $(document.createElement('div'));
+		countryDiv.addClass('country');
+		countryDiv.append(data[i].name+ ' ');
+
+		let countryLink = $(document.createElement('a'));
+		countryLink.attr('href', "country.html?code=" + data[i].alpha3Code);
+		countryLink.text('Read more');
+
+		countryDiv.append(countryLink);
+
+		container.append(countryDiv);
+	}
+}
+
+
+function jqueryParseData(response, status) {
+	console.log(response);
+	console.log(status);
+	renderData(response);
+}
+
+function jqueryAjaxError(response, status) {
+	console.log(response);
+	console.log(status);
+	console.log('error');
+
+
+}
+
+function jqueryLoadIndex(){
+	$.ajax({
+		url: indexLink,
+		method: 'GET',
+		success: jqueryParseData,
+		error: jqueryAjaxError
+	});
+}
+
+$(document).ready(function() {
+	jqueryLoadIndex();
+});
